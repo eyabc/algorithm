617a9a355a8b1ac5110a0f02fb97067238d07d35
eyabc <bey4314@naver.com>
2019-08-28 21:05:43 +0900

    new repo

---
+++ 모의고사.js
@@ -0,0 +1,32 @@
+function solution(answers) {
+    var answer = [];
+    const pattern = {
+        '1':[1,2,3,4,5],
+        '2':[2, 1, 2, 3, 2, 4, 2, 5],
+        '3':[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    }
+    const cnt = [
+        [1, 0],
+        [2, 0],
+        [3, 0]
+    ]
+
+    answers.forEach((i, k) => {
+        if(i === pattern['1'][k % pattern['1'].length]) cnt[0][1] ++;
+        if(i === pattern['2'][k % pattern['2'].length]) cnt[1][1] ++;
+        if(i === pattern['3'][k % pattern['3'].length]) cnt[2][1] ++;
+    })
+
+    cnt.sort((a,b) => b[1] - a[1])
+    console.log(cnt)
+    answer.push(cnt[0][0])
+    
+    
+    
+    if(cnt[0][1] === cnt[1][1]) answer.push(cnt[1][0])
+    if(cnt[0][1] === cnt[1][1] && cnt[1][1] === cnt[2][1]) answer.push(cnt[2][0])
+    
+    return answer;
+
+}
+console.log(solution([5,1,1,1,1,1,2,3,2,1]))
