a7c71a39dd1012c8f9e11e53cc362883770e4c37
eyabc <bey4314@naver.com>
2019-08-31 00:01:52 +0900

    핸드폰 번호 가리기

--- 핸드폰 번호 가리기.js
+++ 핸드폰 번호 가리기.js
@@ -2,4 +2,16 @@ function solution(phone_number) {
     let len = phone_number.length
     return Array(len-3).join("*") + phone_number.slice(len-4, len)
 }
+
+
+function hide_numbers(s) {
+    return s.replace(/\d(?=\d{4})/g, "*");
+}
+
+function hide_numbers(s){
+    var result = "*".repeat(s.length - 4) + s.slice(-4);
+    //함수를 완성해주세요
+  
+    return result;
+}
 console.log(solution('01033334444'))
