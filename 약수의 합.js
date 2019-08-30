d699b855f28013afe706392c4f5d444723267b3a
eyabc <bey4314@naver.com>
2019-08-30 16:22:29 +0900

    4

---
+++ 약수의 합.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = n;
+    let i = 2
+    if (n>1) {
+        answer += 1
+        for(;i<n;i++) {
+            n % i === 0 ? answer +=i : ''
+        }
+    }
+
+    return answer;
+}
+console.log(solution(12))
